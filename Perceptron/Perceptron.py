f52a9d15e86d45a7d70b32b09218fbb30ca007a2
dalong23 <83096606+longda932@users.noreply.github.com>
2021-11-01 20:42:50 -0600

    Add files via upload

--- Perceptron/Perceptron.py
+++ Perceptron/Perceptron.py
@@ -24,6 +24,12 @@ def load_data(shuffle):
     X1=dataTorch1[:,:-1]
     Y1=dataTorch1[:,-1]
 
+    for i in range(X1.shape[0]):
+        if(Y1[i]==0.0):
+            Y1[i]=-1.0
+    
+
+
     X1=torch.cat((X1,torch.ones(X1.shape[0],1)),1)
     data1=torch.cat((X1,Y1.view(-1,1)),1).float()
 
@@ -32,7 +38,9 @@ def load_data(shuffle):
     dataTorch2=torch.from_numpy(rawdata2.values).float()
     X2=dataTorch2[:,:-1]
     Y2=dataTorch2[:,-1]
-
+    for i in range(X2.shape[0]):
+        if(Y2[i]==0.0):
+            Y2[i]=-1.0
     X2=torch.cat((X2,torch.ones(X2.shape[0],1)),1)
     data2=torch.cat((X2,Y2.view(-1,1)),1).float()
 
@@ -66,9 +74,9 @@ def prediction_error(set,weights):
     right=0
     wrong=0
     for i in range(n_examples):
-        if(torch.mm(weights.t(),set[i,:-1].view(-1,1))>0 and set[i,-1]>0):
+        if(torch.mm(weights.t(),set[i,:-1].view(-1,1))>0 and set[i,-1]==1.0):
             right=right+1
-        elif(torch.mm(weights.t(),set[i,:-1].view(-1,1))<=0 and set[i,-1]==0.0):
+        elif(torch.mm(weights.t(),set[i,:-1].view(-1,1))<=0 and set[i,-1]==-1.0):
             right=right+1
         else:
             wrong=wrong+1
@@ -114,10 +122,10 @@ def voted_prediction_error(set,t_c,t_dict,m):
     right=0
     wrong=0
     for i in range(n_examples):
-        if(voted_pred_helper(c,dict,set[i,:-1].view(-1,1))>0 and set[i,-1]>0):
+        if(voted_pred_helper(c,dict,set[i,:-1].view(-1,1))>0 and set[i,-1]==1.0):
             # print("HERE")
             right=right+1
-        elif(voted_pred_helper(c,dict,set[i,:-1].view(-1,1))<=0 and set[i,-1]==0.0):
+        elif(voted_pred_helper(c,dict,set[i,:-1].view(-1,1))<=0 and set[i,-1]==-1.0):
             # print("TERE")
             right=right+1
         else:
@@ -167,9 +175,9 @@ def averaged_prediction_error(set,t_dict,m):
     right=0
     wrong=0
     for i in range(n_examples):
-        if(averaged_pred_helper(dict,set[i,:-1].view(-1,1))>0 and set[i,-1]>0):
+        if(averaged_pred_helper(dict,set[i,:-1].view(-1,1))>0 and set[i,-1]==1.0):
             right=right+1
-        elif(averaged_pred_helper(dict,set[i,:-1].view(-1,1))<=0 and set[i,-1]==0.0):
+        elif(averaged_pred_helper(dict,set[i,:-1].view(-1,1))<=0 and set[i,-1]==-1.0):
             right=right+1
         else:
             wrong=wrong+1
@@ -187,7 +195,12 @@ def averaged_pred_helper(dict,point):
         return -1.0
 
 
-train_data,test_data,train_loader=load_data(False)
+
+
+
+
+
+train_data,test_data,train_loader=load_data(True)
 
 
 weights=standardPerceptron(10,1,train_loader)
@@ -196,20 +209,28 @@ print("Weight for standard perceptron with learning rate of 0.5:",weights)
 error=prediction_error(test_data,weights)
 print("Prediction error:",error)
 
-train_data,test_data,train_loader=load_data(False)
-c,dict,m=voted_perceptron(10,1,train_loader)
 
+c,dict,m=voted_perceptron(10,1,train_loader)
+# print(m)
 
 error1=voted_prediction_error(test_data,c,dict,m)
-print(dict[:m+1,:])
-print(c[:m+1,0])
+dict=dict[:1+m,:]
+c=c[:1+m,:]
 print(error1)
+dict_np = dict.numpy()
+df = pd.DataFrame(dict_np)
+df.to_csv("weights",index=False)
 
-
+c_np = c.numpy()
+df = pd.DataFrame(c_np)
+df.to_csv("counts",index=False)
 
 
 dict,m=averaged_perceptron(10,1,train_loader)
 error1=averaged_prediction_error(test_data,dict,m)
+# print(m)
 print(dict[:m+1,:])
 print(error1)
+
+
 
