484ec4735b0ff4681b493132e29c26eb79a4a60c
dalong23 <83096606+longda932@users.noreply.github.com>
2021-11-19 21:34:38 -0700

    Add files via upload

--- SVM/run.sh
+++ SVM/run.sh
@@ -2,4 +2,4 @@
 pip3 install torch torchvision torchaudio
 pip3 install pandas
 python -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
-python3 Perceptron.py
+python3 SVM.py
