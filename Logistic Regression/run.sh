ea524ff2e80298d608a25bb9d5278e1765ea9925
dalong23 <83096606+longda932@users.noreply.github.com>
2021-12-10 16:52:58 -0700

    Add files via upload

---
+++ Logistic Regression/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pip3 install torch torchvision torchaudio
+pip3 install pandas
+python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+python3 logisticR.py
---
+++ Neural Networks/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+pip3 install torch torchvision torchaudio
+pip3 install pandas
+python3 -m pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose
+python3 DNN.py
+python3 pytorchDNN.py
